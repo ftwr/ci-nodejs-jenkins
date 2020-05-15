8b826d58ee962aabc35277a6838ad72e1b75bc74
ftwr <34319670+ftwr@users.noreply.github.com>
2020-05-15 11:24:16 +0300

    upd final

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,7 +3,7 @@ ssh ubuntu@54.93.254.18 <<EOF
  git clone https://github.com/ftwr/ci-nodejs-jenkins.git
  cd ~/ci-nodejs-jenkins
  git pull
- npm install — production
+ npm install
  pm2 restart all
  exit
 EOF
